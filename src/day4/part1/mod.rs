f1b5e122c1913a2f51fb5a3965458e90ebc4f973
Alejo Abdala <alejo.abdala@mulesoft.com>
2021-12-14 14:52:56 -0300

    Day4 part2 (failing)

--- src/day4/part1/mod.rs
+++ src/day4/part1/mod.rs
@@ -1,30 +1,5 @@
-use std::{convert::TryInto, fmt::Debug};
-
 pub fn solution(input: &str) -> String {
-    let mut lines = input.split_terminator('\n');
-    let numbers: Vec<i32> = lines
-        .find(|_| true)
-        .unwrap()
-        .split_terminator(',')
-        .map(|each| each.parse::<i32>().unwrap())
-        .collect();
-
-    let mut boards: Vec<Board> = lines
-        .skip(1)
-        .fold(vec![String::new()], |mut board_strs, each| {
-            if each.is_empty() {
-                // build board
-                board_strs.push(String::new());
-            } else {
-                let len = board_strs.len();
-                let last_board = &board_strs[len - 1];
-                board_strs[len - 1] = last_board.to_owned() + "\n" + each;
-            }
-            board_strs
-        })
-        .iter()
-        .map(|each| Board::from(each))
-        .collect();
+    let (numbers, mut boards) = super::parse_bingo(input);
 
     for number in numbers {
         if let Some((winner, _)) = boards
@@ -39,67 +14,4 @@ pub fn solution(input: &str) -> String {
         }
     }
     String::from(":(")
-}
-
-#[derive(Debug)]
-struct Board {
-    contents: [[(i32, bool); 5]; 5],
-}
-
-impl Board {
-    fn from(contents: &str) -> Self {
-        let contents: Vec<[(i32, bool); 5]> = contents
-            .split_terminator('\n')
-            .filter(|each| !each.is_empty())
-            .map(|line| {
-                to_5_array(
-                    line.split_ascii_whitespace()
-                        .map(|num_str| (num_str.parse::<i32>().unwrap(), false))
-                        .collect(),
-                )
-            })
-            .collect();
-        Self {
-            contents: to_5_array(contents),
-        }
-    }
-
-    fn result(&self) -> i32 {
-        let mut result = 0;
-        for i in 0..5 {
-            for j in 0..5 {
-                if !self.contents[i][j].1 {
-                    result += self.contents[i][j].0;
-                }
-            }
-        }
-        result
-    }
-
-    fn play(&mut self, number: i32) -> bool {
-        for i in 0..5 {
-            for j in 0..5 {
-                if self.contents[i][j].0 == number {
-                    self.contents[i][j].1 = true;
-                    // check row
-                    if self.contents[i].iter().all(|(_, marked)| *marked) {
-                        return true;
-                    }
-                    // check column
-                    if (0..5).map(|_j| self.contents[i][_j].1).all(|marked| marked) {
-                        return true;
-                    }
-                }
-            }
-        }
-        false
-    }
-}
-
-fn to_5_array<T>(v: Vec<T>) -> [T; 5]
-where
-    T: Debug,
-{
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", 5, v.len()))
 }
--- src/day4/part2/mod.rs
+++ src/day4/part2/mod.rs
@@ -1,3 +1,26 @@
+use super::Board;
+
 pub fn solution(input: &str) -> String {
-  String::from("TODO")
+    let (numbers, mut boards) = super::parse_bingo(input);
+
+    let mut last_winner: Option<&Board> = Option::None;
+    for number in numbers {
+        if let Some((winner, _)) = boards
+            .iter_mut()
+            .map(|b| {
+                let won = b.play(number);
+                (b, won)
+            })
+            .rev()
+            .find(|(_, won)| *won)
+        {
+            last_winner = Option::Some(winner);
+        }
+    }
+
+    if let Some(winner) = last_winner {
+        format!("{}", winner.result())
+    } else {
+        ":(".into()
+    }
 }
