1362db198929051deccdfb504b18fe76d64aba75
Alejo Abdala <alejo.abdala@mulesoft.com>
2021-12-13 21:18:21 -0300

    Day 4/part 1

---
+++ src/day4/part2/mod.rs
@@ -0,0 +1,3 @@
+pub fn solution(input: &str) -> String {
+  String::from("TODO")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod utils;
 
 fn main() {
     println!("day 1:\n{}", day1::solution(&utils::read_input_file("day1")));
     println!("day 2:\n{}", day2::solution(&utils::read_input_file("day2")));
     println!("day 3:\n{}", day3::solution(&utils::read_input_file("day3")));
+    println!("day 4:\n{}", day4::solution(&utils::read_input_file("day4")));
 }
