77a7bf9c683edfcaac743a387a046948741444c6
Alejo Abdala <alejo.abdala@mulesoft.com>
2022-03-30 18:54:56 -0300

    Day 15 part 2

--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -1,6 +1,11 @@
 mod part1;
 mod part2;
 
+use std::{
+    collections::{BTreeMap, HashMap},
+    fmt::Display,
+};
+
 use crate::utils;
 
 pub fn solution(input: &str) -> String {
@@ -24,8 +29,178 @@ mod tests {
     }
 
     #[test]
+    fn part2_example() {
+        let result = part2::solution(&utils::read_example_file("day15"));
+        assert_eq!(result, "315");
+    }
+
+    #[test]
     fn part2() {
         let result = part2::solution(&utils::read_input_file("day15"));
-        assert_eq!(result, "");
+        assert_eq!(result, "2823");
+    }
+}
+
+fn read_cavemap(input: &str) -> CaveMap {
+    let map = CaveMap::from(
+        input
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|c| String::from(c).parse::<u8>().expect("invalid risk level"))
+                    .collect::<Vec<u8>>()
+            })
+            .collect::<Vec<Vec<u8>>>(),
+    );
+    map
+}
+
+fn find_path_risk(map: CaveMap) -> String {
+    let target_pos = map.target_pos();
+    let mut open_set = OpenSet::from(((0, 0), 0, target_pos.0 + target_pos.1));
+    let mut steps = 0;
+    loop {
+        let node @ (cur_pos, risk_acc, _) = open_set.next();
+
+        if cur_pos == target_pos {
+            dbg!(steps);
+            return format!("{}", risk_acc);
+        }
+
+        let moves = vec![
+            map.move_left(node),
+            map.move_right(node),
+            map.move_up(node),
+            map.move_down(node),
+        ];
+
+        moves
+            .into_iter()
+            .flatten()
+            .for_each(|new_node| open_set.offer(new_node));
+        steps += 1;
+    }
+}
+
+type Node = ((usize, usize), usize, usize);
+
+struct OpenSet {
+    est_risk_to_node: BTreeMap<usize, Vec<Node>>,
+    pos_to_lowest_risk: HashMap<(usize, usize), usize>,
+}
+
+impl OpenSet {
+    fn from(initial: Node) -> Self {
+        let mut est_risk_to_node = BTreeMap::new();
+        est_risk_to_node.insert(initial.2, vec![initial]);
+        let pos_to_lowest_risk = HashMap::new();
+        OpenSet {
+            est_risk_to_node,
+            pos_to_lowest_risk,
+        }
+    }
+
+    fn next(&mut self) -> Node {
+        let first_key = self.est_risk_to_node.keys().next().unwrap();
+        let nodes = self.est_risk_to_node.get(first_key).unwrap();
+        let node = nodes[0].to_owned();
+        let key = first_key.to_owned();
+        if nodes.len() == 1 {
+            self.est_risk_to_node.remove(&key);
+        } else {
+            let mut new_nodes = nodes.to_vec();
+            new_nodes.remove(0);
+            self.est_risk_to_node.insert(key, new_nodes);
+        }
+        node
+    }
+
+    fn insert_node(&mut self, new_node: Node) {
+        self.est_risk_to_node
+            .entry(new_node.2)
+            .or_default()
+            .push(new_node);
+        self.pos_to_lowest_risk.insert(new_node.0, new_node.1);
+    }
+
+    fn offer(&mut self, new_node: Node) {
+        let lowest_risk = self.pos_to_lowest_risk.get(&new_node.0);
+        if let Some(risk) = lowest_risk {
+            if new_node.1 < *risk {
+                self.insert_node(new_node);
+            }
+        } else if lowest_risk.is_none() {
+            self.insert_node(new_node);
+        }
+    }
+}
+
+struct CaveMap {
+    map: Vec<Vec<u8>>,
+    max_x: usize,
+    max_y: usize,
+}
+
+impl CaveMap {
+    fn from(map: Vec<Vec<u8>>) -> Self {
+        CaveMap {
+            map: map.clone(),
+            max_y: map.len() - 1,
+            max_x: map[0].len() - 1,
+        }
+    }
+
+    fn target_pos(&self) -> (usize, usize) {
+        (self.max_x, self.max_y)
+    }
+
+    fn move_right(&self, ((x, y), risk_acc, _): Node) -> Option<Node> {
+        if x >= self.max_x {
+            return None;
+        }
+        self.build_node(risk_acc, y, x + 1)
+    }
+
+    fn move_left(&self, ((x, y), risk_acc, _): Node) -> Option<Node> {
+        if x == 0 {
+            return None;
+        }
+        self.build_node(risk_acc, y, x - 1)
+    }
+
+    fn move_up(&self, ((x, y), risk_acc, _): Node) -> Option<Node> {
+        if y == 0 {
+            return None;
+        }
+        self.build_node(risk_acc, y - 1, x)
+    }
+
+    fn move_down(&self, ((x, y), risk_acc, _): Node) -> Option<Node> {
+        if y >= self.max_y {
+            return None;
+        }
+        self.build_node(risk_acc, y + 1, x)
+    }
+
+    fn build_node(&self, risk_acc: usize, new_y: usize, new_x: usize) -> Option<Node> {
+        let new_risk = risk_acc + self.map[new_y][new_x] as usize;
+        Some((
+            (new_x, new_y),
+            new_risk,
+            new_risk as usize + self.max_x - new_x + self.max_y - new_y,
+        ))
+    }
+}
+
+impl Display for CaveMap {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..=self.max_y {
+            for x in 0..=self.max_x {
+                let _ = write!(f, "{} ", self.map[y][x]);
+            }
+            let _ = writeln!(f);
+        }
+
+        Ok(())
     }
 }
--- src/day15/part2/mod.rs
+++ src/day15/part2/mod.rs
@@ -1,3 +1,32 @@
-pub fn solution(_: &str) -> String {
-    "".into()
+use super::{find_path_risk, read_cavemap, CaveMap};
+
+pub fn solution(input: &str) -> String {
+    let map = read_cavemap(input).extend();
+    find_path_risk(map)
+}
+
+impl CaveMap {
+    fn extend(&self) -> Self {
+        let times = 5;
+        let size_x = self.max_x + 1;
+        let size_y = self.max_y + 1;
+        let new_size_x = size_x * times;
+        let new_size_y = size_y * times;
+        let mut new_map = Vec::with_capacity(new_size_y);
+        for y in 0..(new_size_y) {
+            new_map.push(Vec::with_capacity(new_size_x));
+            for x in 0..(new_size_x) {
+                let mod_x = x.rem_euclid(size_x);
+                let mod_y = y.rem_euclid(size_y);
+                let div_x = x.div_euclid(size_x);
+                let div_y = y.div_euclid(size_y);
+                let mut new_risk = self.map[mod_y][mod_x] as usize + div_x + div_y;
+                if new_risk > 9 {
+                    new_risk = new_risk.rem_euclid(9);
+                }
+                new_map[y].push(new_risk as u8);
+            }
+        }
+        CaveMap::from(new_map)
+    }
 }
